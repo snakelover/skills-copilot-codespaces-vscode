8cf57d084f6213ccc07d721034ea0cfd4d9eea4c
Savva <pythontheback@gmail.com>
2023-08-12 13:27:03 +0000

    Copilot fourth commit

--- comments.js
+++ comments.js
@@ -1,13 +1,38 @@
 // Create web server
-var express = require('express');
-var router = express.Router();
-var Comment = require('../models/comment');
-var User = require('../models/user');
-var Post = require('../models/post');
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-// Add comment
-router.post('/', function(req, res) {
-  var comment = new Comment({
-    content: req.body.content,
-    post: req.body.post,
+const app = express();
+app.use(bodyParser.json());
+
+// Create event bus
+const events = [];
+
+// Post event
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    events.push(event);
+
+    // Send event to post service
+    axios.post('http://posts-clusterip-srv:4000/events', event);
+    // Send event to comments service
+    axios.post('http://comments-srv:4001/events', event);
+    // Send event to query service
+    axios.post('http://query-srv:4002/events', event);
+    // Send event to moderation service
+    axios.post('http://moderation-srv:4003/events', event);
+
+    res.send({ status: 'OK' });
+});
+
+// Get events
+app.get('/events', (req, res) => {
+    res.send(events);
+});
+
+// Listen to port
+app.listen(4005, () => {
+    console.log('Listening on port 4005');
+});
